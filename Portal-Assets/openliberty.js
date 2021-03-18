3ca2d43c35ca629d33faec2f035ca9d4889af5e4
Jakub F Pomykala <jakub.pomykala@hotmail.co.uk>
2021-03-18 14:48:51 +0000

    Updating portal-assets on staging

---
+++ Portal-Assets/openliberty.js
@@ -0,0 +1,66 @@
+function addAccountReqText() {
+    var newUserText = document.querySelector('#login-form')
+
+    if (newUserText) {
+        newUserText.insertAdjacentHTML('afterend', '<span class="text register-explain">To prevent abuse of our cloud hosted environments we require users to authenticate themselves, this can be done via the provided social login options or by creating a account.</span>');
+        newUserText.style.maxWidth = "80%";
+    }
+}
+
+function fixRegisterPage() {
+    var ssoUsed = document.querySelector('.js-auth-warning')
+    var registerForm = document.querySelector('#register')
+    var loginForm = document.querySelector('#login')
+    var signInLink = document.querySelector('.toggle-form')
+
+
+    if (ssoUsed) {
+        if (registerForm) {
+            registerForm.style.display = "block";
+        }
+        if (loginForm) {
+            loginForm.style.display = "block";
+        }
+        if (signInLink){
+            signInLink.innerHTML = '';
+        }
+    }
+}
+
+function changePageText() {
+    var signIn = document.querySelector('#login-form h2')
+    var emailReminder = document.querySelector('#login-email-desc')
+    var createAccount = document.querySelector('#login-form > div.toggle-form > a')
+    var forgotPassword = document.querySelector('#login > div.form-field.password-password > button')
+
+    if (emailReminder) {
+        emailReminder.textContent = 'The email address you used to create an Open Liberty SkillsNetwork account.'
+    }
+    if (forgotPassword) {
+        forgotPassword.textContent = 'Forgot your password?'
+    }
+}
+Function fixPage() {
+    var warning = document.querySelector('.js-form-feedback .js-auth-warning');
+    
+    if (warning) {
+      document.querySelector('form#login, form#register').style.display = 'flex';
+      document.querySelector('form#login, form#register').style.flexDirection = 'column';
+      document.querySelector('form#login, form#register').style.maxWidth = '400px';
+    }
+}
+
+function addFooter() {
+    var bodyWindow = document.querySelector('body')
+
+    bodyWindow.insertAdjacentHTML('afterend', '<footer> <div> <p> <a rel="noopener" target="_blank" href="https://ide.skillsnetwork.site/legal">Terms of Use</a> | <a rel="noopener" target="_blank" href="https://openliberty.skillsnetwork.site/privacy">Privacy Notice</a> </p> </div> <div class="footer__copyright"> © Copyright Open Liberty Skills Network 2021 </div> </footer>');
+}
+
+
+setTimeout(function() {
+    //addAccountReqText();
+    // fixRegisterPage();
+    changePageText();
+    // addFooter();
+    fixPage();
+},100);
