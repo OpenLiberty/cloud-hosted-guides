913e03f7f140e0503ffa5695b4e1f572d4888a2d
Austin <Austin.Bailey@ibm.com>
2021-04-13 11:39:59 +0100

    Add updated portal lms and liquid theme
    Signed-off-by: Austin Bailey <Austin.Bailey@ibm.com>

--- Portal-Assets/ol-admin-portal-backup/Custom_Footer_Code_Section.js
+++ Portal-Assets/ol-admin-portal-backup/Custom_Footer_Code_Section.js
@@ -1,23 +1,23 @@
-< script >
+<script>
     (function() {
         // Move the 'Launch quicklab button under the content'
-        if (window.location.pathname.startsWith("/quicklab")) {
+        if(window.location.pathname.startsWith("/quicklab")) {
             document.querySelector('.row.course-details-content .col-md-8')
                 .append(document.querySelector('.row.course-details-header form, .row.course-details-header a'));
         }
     })();
 
-(function() {
-    var accessButton = document.querySelector('.btn-quicklabs-action')
-
-    if (accessButton) {
-        accessButton.innerText = "Access cloud-hosted guide";
-    }
-})();
-
-(function() {
-    document.querySelector('.user-link').href = "https://courses.openliberty.skillsnetwork.site/account/settings";
-    document.querySelector(".dropdown-menu a[href='/profile']").innerText = "Profile"
-    document.querySelector(".dropdown-menu a[href='/favorites']").innerText = "Favorites"
-})(); <
-/script>
+    (function() {
+        var accessButton = document.querySelector('.btn-quicklabs-action')
+        
+        if (accessButton) {
+            accessButton.innerText="Access cloud-hosted guide";
+        }
+    })();
+    
+    (function() {
+        document.querySelector('.user-link').href="https://courses.openliberty.skillsnetwork.site/account/settings";
+        document.querySelector(".dropdown-menu a[href='/profile']").innerText="Profile"
+        document.querySelector(".dropdown-menu a[href='/favorites']").innerText="Favorites"
+    })();
+</script>
