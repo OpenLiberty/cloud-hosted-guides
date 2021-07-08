95ad9d524ae46c260fbf1fead55efc4f792dde18
Austin <Austin.Bailey@ibm.com>
2021-07-08 11:13:34 +0100

    Update portal CSS an JS
    Signed-off-by: Austin Bailey <Austin.Bailey@ibm.com>

--- Portal-Assets/ol-admin-portal-backup/Custom_Footer_Code_Section.js
+++ Portal-Assets/ol-admin-portal-backup/Custom_Footer_Code_Section.js
@@ -1,3 +1,9 @@
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTRJ86B"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
 <script>
     (function() {
         // Move the 'Launch quicklab button under the content'
@@ -16,8 +22,51 @@
     })();
     
     (function() {
-        document.querySelector('.user-link').href="https://courses.openliberty.skillsnetwork.site/account/settings";
-        document.querySelector(".dropdown-menu a[href='/profile']").innerText="Profile"
-        document.querySelector(".dropdown-menu a[href='/favorites']").innerText="Favorites"
+        var userLink = document.querySelector('.user-link');
+        
+        if (userLink) {
+            userLink.href="https://courses.openliberty.skillsnetwork.site/account/settings";
+            document.querySelector(".dropdown-menu a[href='/profile']").innerText="Profile"
+            document.querySelector(".dropdown-menu a[href='/favorites']").innerText="Favorites"
+        }
     })();
+</script>
+
+<script>
+    // Rating page scripts
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    var guideName = (urlParams.get('guide-name') ? urlParams.get('guide-name') : "")
+    var guideID = (urlParams.get('guide-id') ? urlParams.get('guide-id') : "")
+    
+    if (guideName) {
+        guideName = guideName.replace("/", "")
+        guideName = guideName.replace("\\", "")
+    }
+    
+    document.querySelector('#guide-name').outerHTML = "<h3 id=\"guide-name\">What did you think of the <em>" + guideName+ "</em> guide?</h3>"
+    document.querySelector('#content > div > div > p:nth-child(5) > a').href += guideID + '/issues'
+    document.querySelector('#content > div > div > p:nth-child(6) > a').href += guideID + '/pulls'
+    
+    var popup = document.getElementById("feedbackPopup");
+    var closeButton = document.getElementById("close_button");
+    
+    document.querySelectorAll("#feedback_ratings img").forEach(face => 
+        face.onclick = () => {
+            if (guideName) {
+                popup.style.display = "block";
+                face.style.opacity = 1;
+            }
+        }
+    )
+    
+    closeButton.onclick = function() {
+        popup.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
 </script>
