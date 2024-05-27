bd545a60cae8d7620975fe5e3c2480bf74feaef4
EricCH <erichaocr@gmail.com>
2024-05-26 21:52:29 -0400

    feat: Update AWB URL in mirrorGuide.yml

--- .github/workflows/deploy_guide.sh
+++ .github/workflows/deploy_guide.sh
@@ -8,15 +8,21 @@ CHANGE_LOG=$4
 PUBLISH=$5
 API_URL=$6
 
-response=$(curl -s -X POST "${API_URL}/api/v1/labs/${LAB_ID}/lab_versions/latest/drafts" \
+response=$(curl -s -w "%{http_code}" -o response.json -X POST "${API_URL}/api/v1/labs/${LAB_ID}/lab_versions/latest/drafts" \
   -H "Authorization: Bearer ${OAUTH_TOKEN}" \
   -F "file=@${FILE_PATH}" \
   -F "draft[changelog]=${CHANGE_LOG}" \
   -F "publish=${PUBLISH}")
 
-if [[ "$response" -ne 200 && "$response" -ne 201 ]]; then
-  echo "Request failed with status $response"
+http_status=$(tail -n1 <<< "$response")
+response_body=$(head -n-1 response.json)
+
+if [[ "$http_status" -ne 200 && "$http_status" -ne 201 ]]; then
+  echo "Request failed with status $http_status"
+  echo "Response Body: $response_body"
   exit 1
 else
-  echo "Request succeeded."
+  echo "Request succeeded with status $http_status"
 fi
+
+rm response.json
